5f47ab3489ef5ba9f43fbae0988aa0fbd546d28a
Derik Hammer <5934523+sqlhammer@users.noreply.github.com>
2026-07-25 23:17:21 -0400

    Created a new DELETE /api/accounts/me endpoint

---
+++ backend/EventManager.Api/Persistence/Migrations/20260726025407_AccountSoftDelete.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManager.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUsers");
+        }
+    }
+}
